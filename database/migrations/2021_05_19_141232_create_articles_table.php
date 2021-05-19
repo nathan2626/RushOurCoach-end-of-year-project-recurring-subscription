3d35c2e8c4ffdff164b7a87e1f5543ad055a6f4b
nathan2626 <61419599+nathan2626@users.noreply.github.com>
2021-05-19 16:24:10 +0200

    Creating the articles migration 🎉

---
+++ database/migrations/2021_05_19_141232_create_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('image');
+            $table->string('title');
+            $table->boolean('published')->default(0);
+            $table->dateTime('date_of_publication');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
