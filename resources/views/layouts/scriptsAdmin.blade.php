5d364bdb34d4e711db62ba5c4e6a689e6b6b801d
nathan2626 <nathanjourno@yahoo.fr>
2021-05-25 18:32:59 +0200

    Admin Part : debug in articles view

--- resources/views/layouts/scriptsAdmin.blade.php
+++ resources/views/layouts/scriptsAdmin.blade.php
@@ -1,3 +1,4 @@
+
 <script src="{{asset('/assets-admin/js/modernizr.min.js')}}"></script>
 <script src="{{asset('/assets-admin/js/jquery.min.js')}}"></script>
 <script src="{{asset('/assets-admin/js/moment.min.js')}}"></script>
@@ -10,5 +11,19 @@
 <script src="{{asset('/assets-admin/js/jquery.blockUI.js')}}"></script>
 <script src="{{asset('/assets-admin/js/jquery.nicescroll.js')}}"></script>
 
+<!-- Counter-Up-->
+<script src="{{asset('assets-admin/plugins/waypoints/lib/jquery.waypoints.min.js')}}"></script>
+<script src="{{asset('assets-admin/plugins/counterup/jquery.counterup.min.js')}}"></script>
+
 <!-- App js -->
 <script src="{{asset('/assets-admin/js/admin.js')}}"></script>
+
+<script>
+    $(document).on('ready', function() {
+        // counter-up
+        $('.counter').counterUp({
+            delay: 10,
+            time: 600
+        });
+    });
+</script>
